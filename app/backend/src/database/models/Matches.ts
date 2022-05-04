9e5fdcf6acd83ec6335f0dea9d7c8b79c0edfeac
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-03 22:02:54 -0300

    feat : implementa model da tabela matches

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,65 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
+
+class Matches extends Model {
+  public id: number;
+
+  public homeTeam: number;
+
+  public homeTeamGoals: number;
+
+  public awayTeam: number;
+
+  public awayTeamGoals: number;
+
+  public inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Teams.hasMany(Matches, { foreignKey: 'homeTeam', as: 'homeMatches' });
+Teams.hasMany(Matches, { foreignKey: 'awayTeam', as: 'awayMatches' });
+
+Matches.belongsTo(
+  Teams,
+  { foreignKey: 'homeTeam', as: 'homeTeamId', onDelete: 'CASCADE', onUpdate: 'CASCADE' },
+);
+Matches.belongsTo(
+  Teams,
+  { foreignKey: 'awayTeam', as: 'awayTeamId', onDelete: 'CASCADE', onUpdate: 'CASCADE' },
+);
+
+export default Matches;
