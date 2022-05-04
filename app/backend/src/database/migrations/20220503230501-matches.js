fd6d949a39ecb8c35687cc541b10f10ee8ccfcf5
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-03 21:10:58 -0300

    feat : implementa migration da tabela matches

---
+++ app/backend/src/database/migrations/20220503230501-matches.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'Teams',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        reference: {
+          model: 'Teams',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      in_progress: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('matches');
+  }
+};
