9a77dfd2733616f7bf4b6b89511a19ac2d2d6d55
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-04 21:23:46 -0300

    chore : edita nome do arquivo e interface

--- app/backend/src/interfaces/IPayloadLogin .ts
+++ app/backend/src/interfaces/IPayloadLogin .ts
@@ -1,4 +1,4 @@
-export interface IPayloadToken {
+export interface IPayloadLogin {
   email: string,
   password: string
 }
--- app/backend/src/jwt/tokenGenerate.ts
+++ app/backend/src/jwt/tokenGenerate.ts
@@ -1,8 +1,8 @@
 import * as fs from 'fs/promises';
 import { sign } from 'jsonwebtoken';
-import { IPayloadToken } from '../interfaces/payloadJwt';
+import { IPayloadLogin } from '../interfaces/IPayloadLogin ';
 
-const signUser = async (payload:IPayloadToken, duration = '7d'): Promise<string> => {
+const signUser = async (payload:IPayloadLogin, duration = '7d'): Promise<string> => {
   const SECRET_KEY = await fs.readFile('./jwt.evaluation.key', 'utf-8');
   const token = sign(payload, SECRET_KEY, {
     algorithm: 'HS256',
