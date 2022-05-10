9e77f9d03ca18e311c877b1f61f06d77ecad56c9
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-09 21:45:19 -0300

    feat : add o middleware na rota login

--- app/backend/src/routers/loginRouter.ts
+++ app/backend/src/routers/loginRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
+import validatePayload from '../middleware/login.validate';
 
 const usersController = new UsersController();
 const loginRoute = Router();
 
-loginRoute.post('/', usersController.login);
+loginRoute.post('/', validatePayload, usersController.login);
 
 export default loginRoute;
