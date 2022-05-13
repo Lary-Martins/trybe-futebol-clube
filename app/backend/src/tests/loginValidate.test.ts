c080be8edf0368739c2b6c01a340725c4940b380
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-13 16:36:01 -0300

    test: implementa teste de caso  - token inválido

--- app/backend/src/tests/loginValidate.test.ts
+++ app/backend/src/tests/loginValidate.test.ts
@@ -19,3 +19,19 @@ const userMock = {
   password: 'senha_certa',
 };
 
+
+describe('Teste de integração da rota login/validate', () => {
+  let chaiHttpResponse: Response;
+  describe('Verifica o retorno em casos de inputs incorretos', () => {
+    it('1 - Caso seja passado um token inválido', async () => {
+      chaiHttpResponse =  await chai.request(app).get('/login/validate').set('authorization', 'invalid_token');
+
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(chaiHttpResponse.body).to.be.an('object');
+      expect(chaiHttpResponse.body).to.have.property('message');
+      expect(chaiHttpResponse.body.message).to.be.equal(
+        'Expired or invalid token',
+      );
+    });
+  });
+});
