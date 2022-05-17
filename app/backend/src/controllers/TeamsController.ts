d7c1bea73afdcbce0cd345e7d48572b5a40b1c4b
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-17 11:15:03 -0300

    feat : implementa classe TeamsController e método getAllTeams

---
+++ app/backend/src/controllers/TeamsController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import teamsFactory from '../factory/teamsFactory';
+
+class TeamsController {
+  private teamsService = teamsFactory;
+
+  constructor() {
+    this.getAllTeams = this.getAllTeams.bind(this);
+  }
+
+  async getAllTeams(_req: Request, res: Response) {
+    try {
+      const response = await this.teamsService.getAllTeams();
+
+      res.status(response.code).json(response.data.team);
+    } catch (err) {
+      const message = err as string;
+      throw new Error(message);
+    }
+  }
+}
+
+export default TeamsController;
