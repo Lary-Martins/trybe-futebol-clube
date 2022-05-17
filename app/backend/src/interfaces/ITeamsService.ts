78e2158fd4d812ad515e7c207eb8aeeb60eeac9c
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-17 10:46:26 -0300

    feat : cria interface para classe TeamsService

---
+++ app/backend/src/interfaces/ITeamsService.ts
@@ -0,0 +1,14 @@
+import { StatusCodes } from 'http-status-codes';
+import { ITeams } from './ITeams';
+
+interface ITeamsReturn {
+  code: StatusCodes,
+  data: {
+    team: ITeams | ITeams[],
+    message?: string,
+  }
+}
+
+export interface ITeamsService {
+  getAllTeams(): Promise <ITeamsReturn>
+}
