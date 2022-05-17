c3fa61f0f9d8f239e5fa1f683fda29a0178af72f
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-17 12:42:49 -0300

    test : implementa test de caso - exista inserções no BD

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -13,13 +13,84 @@ const { expect } = chai;
 
 const teamsArrMock = [
   {
-    id: 1,
-    teamName: 'Flamengo',
+      "id": 1,
+      "teamName": "Avaí/Kindermann"
   },
   {
-    id: 10,
-    teamName: 'Vasco',
+      "id": 2,
+      "teamName": "Bahia"
   },
-];
+  {
+      "id": 3,
+      "teamName": "Botafogo"
+  },
+  {
+      "id": 4,
+      "teamName": "Corinthians"
+  },
+  {
+      "id": 5,
+      "teamName": "Cruzeiro"
+  },
+  {
+      "id": 6,
+      "teamName": "Ferroviária"
+  },
+  {
+      "id": 7,
+      "teamName": "Flamengo"
+  },
+  {
+      "id": 8,
+      "teamName": "Grêmio"
+  },
+  {
+      "id": 9,
+      "teamName": "Internacional"
+  },
+  {
+      "id": 10,
+      "teamName": "Minas Brasília"
+  },
+  {
+      "id": 11,
+      "teamName": "Napoli-SC"
+  },
+  {
+      "id": 12,
+      "teamName": "Palmeiras"
+  },
+  {
+      "id": 13,
+      "teamName": "Real Brasília"
+  },
+  {
+      "id": 14,
+      "teamName": "Santos"
+  },
+  {
+      "id": 15,
+      "teamName": "São José-SP"
+  },
+  {
+      "id": 16,
+      "teamName": "São Paulo"
+  }
+]
 
-describe('Teste de integração da rota teams', () => {});
+describe('Teste de integração da rota teams', () => {
+  let chaiHttpResponse: Response;
+  describe('Verifica se os dados corretos são retornados', () => {
+    it('1 - Caso exista inserções no Banco de Dados, deve retornar um array com os dados', async () => {
+      before(async () => {
+        sinon.stub(Teams, 'findAll').resolves(teamsArrMock as Teams[]);
+      });
+
+      chaiHttpResponse = await chai.request(app).get('/teams');
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+      expect(chaiHttpResponse.body).to.be.an('array');
+      expect(chaiHttpResponse.body).to.be.have.length(16);
+    });
+  });
+});
