b35c525f2b30c7cde7ec2d4d835784c6cc8a4042
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-17 10:04:15 -0300

    feat : cria classe TeamsRepository e método findAllTeams

---
+++ app/backend/src/repositories/TeamsRepository.ts
@@ -0,0 +1,18 @@
+import { ITeamsRepository } from '../interfaces/ITeamsRepository';
+import Teams from '../database/models/Teams';
+
+class TeamsRepository implements ITeamsRepository {
+  private teamsModel = Teams;
+
+  async findAllTeams() {
+    try {
+      const data = await this.teamsModel.findAll();
+      return data;
+    } catch (err) {
+      const message = err as string;
+      throw new Error(message);
+    }
+  }
+}
+
+export default TeamsRepository;
