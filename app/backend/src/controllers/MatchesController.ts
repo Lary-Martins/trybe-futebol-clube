e3b1d4423af9c608ba5f00aa340031d6fe5cde24
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-23 20:51:11 -0300

    feat : implementa método de postNewMatche na classe MatchesController

--- app/backend/src/controllers/MatchesController.ts
+++ app/backend/src/controllers/MatchesController.ts
@@ -8,9 +8,27 @@ class MatchesController {
     this.getAllMatches = this.getAllMatches.bind(this);
   }
 
-  async getAllMatches(_req: Request, res:Response) {
+  async getAllMatches(_req: Request, res: Response) {
     try {
       const response = await this.matchesService.getAllMatches();
+
+      res.status(response.code).json(response.data);
+    } catch (err) {
+      const message = err as string;
+      throw new Error(message);
+    }
+  }
+
+  async postNewMatche(req: Request, res: Response) {
+    const { awayTeam, homeTeam, homeTeamGoals, awayTeamGoals, inProgress } = req.body;
+    try {
+      const response = await this.matchesService.postNewMatche({
+        awayTeam,
+        homeTeam,
+        homeTeamGoals,
+        awayTeamGoals,
+        inProgress,
+      });
 
       res.status(response.code).json(response.data);
     } catch (err) {
