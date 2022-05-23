fb3bc888185b6de928c3d8ccc3c7bd071b80013c
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-23 20:05:23 -0300

    feat : cria interface do método createNewMatche da classe MatchesRepository

--- app/backend/src/interfaces/matchesInterfaces/IMatchesRepository.ts
+++ app/backend/src/interfaces/matchesInterfaces/IMatchesRepository.ts
@@ -1,5 +1,7 @@
 import Matches from '../../database/models/Matches';
+import { IMatcheRequest } from './IMatche';
 
 export interface IMatchesRepository {
   findAllMatches(): Promise <Matches[]>
+  createNewMatche(body: IMatcheRequest): Promise <Matches>
 }
