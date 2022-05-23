f47729704895231a3dbae1973858c226d3638e82
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-23 20:13:51 -0300

    feat : implementa método postNewMatche

--- app/backend/src/services/MatchesService.ts
+++ app/backend/src/services/MatchesService.ts
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import { IMatchesService } from '../interfaces/matchesInterfaces/IMatchesServices';
 import { IMatchesRepository } from '../interfaces/matchesInterfaces/IMatchesRepository';
+import { IMatcheRequest } from '../interfaces/matchesInterfaces/IMatche';
 
 class MatchesService implements IMatchesService {
   private matchesRepository: IMatchesRepository;
@@ -14,6 +15,19 @@ class MatchesService implements IMatchesService {
       const matchesData = await this.matchesRepository.findAllMatches();
 
       return { code: StatusCodes.OK, data: matchesData };
+    } catch (err) {
+      const message = err as string;
+      throw new Error(message);
+    }
+  }
+
+  async postNewMatche(body: IMatcheRequest) {
+    const matchBody = { inProgress: true, ...body };
+    try {
+      const { id } = await this.matchesRepository.createNewMatche(matchBody);
+
+      const newMatche = { id, ...body };
+      return { code: StatusCodes.OK, data: newMatche };
     } catch (err) {
       const message = err as string;
       throw new Error(message);
