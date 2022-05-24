a19028d304e4b45240140e53b859a7be9a7e9950
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-24 14:27:33 -0300

    feat : cria método updateMatchGoals na classe

--- app/backend/src/repositories/MatchesRepository.ts
+++ app/backend/src/repositories/MatchesRepository.ts
@@ -1,19 +1,22 @@
 import { IMatchesRepository } from '../interfaces/matchesInterfaces/IMatchesRepository';
 import Matches from '../database/models/Matches';
 import Teams from '../database/models/Teams';
-import { IMatcheRequest } from '../interfaces/matchesInterfaces/IMatche';
+import {
+  IMatcheRequest,
+  ITeamsGoals,
+} from '../interfaces/matchesInterfaces/IMatche';
 
 class MatchesRepository implements IMatchesRepository {
   private MatchesModel = Matches;
 
   async findAllMatches() {
     try {
-      const data = await this.MatchesModel.findAll(
-        { include: [
+      const data = await this.MatchesModel.findAll({
+        include: [
           { model: Teams, as: 'teamHome', attributes: { exclude: ['id'] } },
           { model: Teams, as: 'teamAway', attributes: { exclude: ['id'] } },
-        ] },
-      );
+        ],
+      });
       return data;
     } catch (err) {
       const message = err as string;
@@ -21,7 +24,7 @@ class MatchesRepository implements IMatchesRepository {
     }
   }
 
-  async createNewMatche(body: IMatcheRequest) {
+  async createNewMatch(body: IMatcheRequest) {
     try {
       const data = await this.MatchesModel.create(body);
       return data;
@@ -31,9 +34,26 @@ class MatchesRepository implements IMatchesRepository {
     }
   }
 
-  async updateProgressMatche(id: number) {
+  async updateMatchProgress(id: number) {
     try {
-      const data = await this.MatchesModel.update({ inProgress: false }, { where: { id } });
+      const data = await this.MatchesModel.update(
+        { inProgress: false },
+        { where: { id } },
+      );
+      return data;
+    } catch (err) {
+      const message = err as string;
+      throw new Error(message);
+    }
+  }
+
+  async updateMatchGoals(teamGoals: ITeamsGoals, id: number) {
+    const { homeTeamGoals, awayTeamGoals } = teamGoals;
+    try {
+      const data = await this.MatchesModel.update(
+        { homeTeamGoals, awayTeamGoals },
+        { where: { id } },
+      );
       return data;
     } catch (err) {
       const message = err as string;
