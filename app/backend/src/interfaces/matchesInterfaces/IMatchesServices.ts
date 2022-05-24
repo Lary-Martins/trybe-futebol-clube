e4f6954a21352933bae2a21e3c41f99e182a638b
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-24 15:13:02 -0300

    feat : implementa novo método na interface da classe MacthesService

--- app/backend/src/interfaces/matchesInterfaces/IMatchesServices.ts
+++ app/backend/src/interfaces/matchesInterfaces/IMatchesServices.ts
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import Matches from '../../database/models/Matches';
-import { IMatchRequest, IMatchResponse } from './IMatch';
+import { IMatchRequest, IMatchResponse, ITeamsGoals } from './IMatch';
 
 interface IMatchesReturn {
   code: StatusCodes;
@@ -17,4 +17,5 @@ export interface IMatchesService {
   getAllMatches(): Promise<IMatchesReturn>;
   postNewMatch(body: IMatchRequest): Promise<IMatchesReturn>
   patchMatchProgress(id: number): Promise<IMatchesReturn>
+  patchMatchGoals(teamGoals: ITeamsGoals, id: number): Promise<IMatchesReturn>
 }
