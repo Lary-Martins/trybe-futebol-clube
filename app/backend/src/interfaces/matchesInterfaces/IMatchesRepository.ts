c4b21ba04862189f7d418b1c66772ba13733e2b5
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-23 23:02:25 -0300

    feat : implmenta método de update na interface IMatchesRepository

--- app/backend/src/interfaces/matchesInterfaces/IMatchesRepository.ts
+++ app/backend/src/interfaces/matchesInterfaces/IMatchesRepository.ts
@@ -4,4 +4,5 @@ import { IMatcheRequest } from './IMatche';
 export interface IMatchesRepository {
   findAllMatches(): Promise <Matches[]>
   createNewMatche(body: IMatcheRequest): Promise <Matches>
+  updateProgressMatche(id: number): Promise <[number, Matches[]]>
 }
