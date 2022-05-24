ca72862b4fc8a02df2cac6b8156231385244ead8
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-23 22:31:27 -0300

    feat : inclui o parametro na factory para a classe MatchesController

--- app/backend/src/factory/matchesFactory.ts
+++ app/backend/src/factory/matchesFactory.ts
@@ -1,6 +1,7 @@
 import MatchesService from '../services/MatchesService';
 import MatchesRepository from '../repositories/MatchesRepository';
+import TeamsRepository from '../repositories/TeamsRepository';
 
-const matchesFactory = new MatchesService(new MatchesRepository());
+const matchesFactory = new MatchesService(new MatchesRepository(), new TeamsRepository());
 
 export default matchesFactory;
