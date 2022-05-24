f653a633474ec32bbcea527162e1dd6627f8f12f
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-23 22:32:51 -0300

    feat : implementa middleware de validação para rota post /matche

--- app/backend/src/routers/matchesRouter.ts
+++ app/backend/src/routers/matchesRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import validadeMatcheBody from '../middleware/matche.validade';
 import MatchesController from '../controllers/MatchesController';
 
 const matchesController = new MatchesController();
 const matchesRouter = Router();
 
 matchesRouter.get('/', matchesController.getAllMatches);
-matchesRouter.post('/', matchesController.postNewMatche);
+matchesRouter.post('/', validadeMatcheBody, matchesController.postNewMatche);
 
 export default matchesRouter;
