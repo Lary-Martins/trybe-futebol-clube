7456ba00c7c7cc52be3111cb2efcd953817581c4
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-25 02:42:19 -0300

    feat : implementa LeaderboardsController

---
+++ app/backend/src/controllers/LeaderboardsController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import leaderboardFactory from '../factory/leaderboardFactory';
+
+class LeaderboardsController {
+  private leaderboardService = leaderboardFactory;
+
+  constructor() {
+    this.getLeaderboardHome = this.getLeaderboardHome.bind(this);
+  }
+
+  async getLeaderboardHome(_req: Request, res: Response) {
+    try {
+      const response = await this.leaderboardService.getLeadboard('homeTeam');
+
+      res.status(response.code).json(response.data);
+    } catch (err) {
+      const message = err as string;
+      throw new Error(message);
+    }
+  }
+
+  async getLeaderboardAway(_req: Request, res: Response) {
+    try {
+      const response = await this.leaderboardService.getLeadboard('awayTeam');
+
+      res.status(response.code).json(response.data);
+    } catch (err) {
+      const message = err as string;
+      throw new Error(message);
+    }
+  }
+}
+
+export default LeaderboardsController;
