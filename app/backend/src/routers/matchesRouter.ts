4b8e00f9279a6c3265fd8b2d4e5924f5c9ff13cc
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-24 22:21:00 -0300

    feat : implementa validação na rota /matches/:id/finish

--- app/backend/src/routers/matchesRouter.ts
+++ app/backend/src/routers/matchesRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { validadeMatchBody, validadeTeamGoals } from '../middleware/match.validade';
 import MatchesController from '../controllers/MatchesController';
+import validateToken from '../middleware/token.validate';
 
 const matchesController = new MatchesController();
 const matchesRouter = Router();
@@ -8,6 +9,6 @@ const matchesRouter = Router();
 matchesRouter.get('/', matchesController.getAllMatches);
 matchesRouter.post('/', validadeMatchBody, matchesController.postNewMatch);
 matchesRouter.patch('/:id', validadeTeamGoals, matchesController.patchMatchGoals);
-matchesRouter.patch('/:id/finish', matchesController.patchMatchProgress);
+matchesRouter.patch('/:id/finish', validateToken, matchesController.patchMatchProgress);
 
 export default matchesRouter;
