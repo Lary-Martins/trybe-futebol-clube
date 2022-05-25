7de3d4ff9e12ba1dc89c58ee048e2bdf680947e7
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-25 04:04:20 -0300

    feat : implementa rota /leaderboard

--- app/backend/src/routers/leaderboard.ts
+++ app/backend/src/routers/leaderboard.ts
@@ -4,6 +4,7 @@ import LeaderboardsController from '../controllers/LeaderboardsController';
 const leaderboardRouter = Router();
 const leadboardController = new LeaderboardsController();
 
+leaderboardRouter.get('/', leadboardController.getLeaderboard);
 leaderboardRouter.get('/home', leadboardController.getLeaderboardHome);
 leaderboardRouter.get('/away', leadboardController.getLeaderboardAway);
 
