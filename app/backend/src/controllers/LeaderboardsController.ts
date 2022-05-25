e17dff3ec69d2e0b1c6863ec796c84ecd9030526
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-25 05:09:21 -0300

    feat  : xo

--- app/backend/src/controllers/LeaderboardsController.ts
+++ app/backend/src/controllers/LeaderboardsController.ts
@@ -1,4 +1,6 @@
+/* eslint-disable max-lines-per-function */
 import { Request, Response } from 'express';
+// import ordenar from '../utils/ordenar';
 import leaderboardFactory from '../factory/leaderboardFactory';
 
 class LeaderboardsController {
@@ -34,23 +36,48 @@ class LeaderboardsController {
 
   async getLeaderboard(_req: Request, res: Response) {
     const away = await this.leaderboardService.getLeadboard('awayTeam');
-    const { data } = await this.leaderboardService.getLeadboard('homeTeam');
+    away.data.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      } if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
 
-    const response = away.data.map((team, i) => ({ name: team.name,
-      totalPoints: team.totalPoints + data[i].totalPoints,
-      totalGames: team.totalGames + data[i].totalGames,
-      totalVictories: team.totalVictories + data[0].totalVictories,
-      totalDraws: team.totalDraws + data[i].totalDraws,
-      totalLosses: team.totalLosses + data[i].totalLosses,
-      goalsFavor: team.goalsFavor + data[i].goalsFavor,
-      goalsOwn: team.goalsOwn + data[i].goalsOwn,
-      goalsBalance: team.goalsFavor + data[i].goalsOwn,
-      efficiency: +(
-        (((team.totalVictories + data[i].totalVictories) * 3
+    const { data } = await this.leaderboardService.getLeadboard('homeTeam');
+    // eslint-disable-next-line sonarjs/no-identical-functions
+    data.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      } if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+
+    const response = away.data.map((team, i) => {
+      const sla = { name: team.name,
+        totalPoints: team.totalPoints + data[i].totalPoints,
+        totalGames: team.totalGames + data[i].totalGames,
+        totalVictories: team.totalVictories + data[0].totalVictories,
+        totalDraws: team.totalDraws + data[i].totalDraws,
+        totalLosses: team.totalLosses + data[i].totalLosses,
+        goalsFavor: team.goalsFavor + data[i].goalsFavor,
+        goalsOwn: team.goalsOwn + data[i].goalsOwn,
+        goalsBalance: team.goalsBalance + data[i].goalsBalance,
+        efficiency: +(
+          (((team.totalVictories + data[i].totalVictories) * 3
          + (data[i].totalDraws + team.totalDraws))
           / ((team.totalGames + data[i].totalGames) * 3)) * 100).toFixed(2),
-    }
-    ));
+      };
+      return sla;
+    });
+    response.sort((a, b) => a.goalsOwn - b.goalsOwn)
+      .sort((a, b) => b.goalsFavor - a.goalsFavor)
+      .sort((a, b) => b.goalsBalance - a.goalsBalance)
+      .sort((a, b) => b.totalVictories - a.totalVictories)
+      .sort((a, b) => b.totalPoints - a.totalPoints);
     res.status(away.code).json(response);
   }
 }
---
+++ app/backend/src/utils/ordenar.ts
@@ -0,0 +1,21 @@
+interface ILeadboard {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: number;
+}
+
+export default (leadboard: ILeadboard[]) => {
+  leadboard.sort((a, b) => a.goalsOwn - b.goalsOwn)
+    .sort((a, b) => b.goalsFavor - a.goalsFavor)
+    .sort((a, b) => b.goalsBalance - a.goalsBalance)
+    .sort((a, b) => b.totalVictories - a.totalVictories)
+    .sort((a, b) => b.totalPoints - a.totalPoints);
+  return leadboard;
+};
