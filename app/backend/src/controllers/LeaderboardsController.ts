650ae439b99075b5348305392be3a8e02a7b0b93
Laryssa Martins <laryssamartinsoffice@gmail.com>
2022-05-25 04:03:49 -0300

    feat : implementa novo método getLeaderboard

--- app/backend/src/controllers/LeaderboardsController.ts
+++ app/backend/src/controllers/LeaderboardsController.ts
@@ -7,6 +7,7 @@ class LeaderboardsController {
   constructor() {
     this.getLeaderboardHome = this.getLeaderboardHome.bind(this);
     this.getLeaderboardAway = this.getLeaderboardAway.bind(this);
+    this.getLeaderboard = this.getLeaderboard.bind(this);
   }
 
   async getLeaderboardHome(_req: Request, res: Response) {
@@ -29,6 +30,28 @@ class LeaderboardsController {
       const message = err as string;
       throw new Error(message);
     }
+  }
+
+  async getLeaderboard(_req: Request, res: Response) {
+    const away = await this.leaderboardService.getLeadboard('awayTeam');
+    const { data } = await this.leaderboardService.getLeadboard('homeTeam');
+
+    const response = away.data.map((team, i) => ({ name: team.name,
+      totalPoints: team.totalPoints + data[i].totalPoints,
+      totalGames: team.totalGames + data[i].totalGames,
+      totalVictories: team.totalVictories + data[0].totalVictories,
+      totalDraws: team.totalDraws + data[i].totalDraws,
+      totalLosses: team.totalLosses + data[i].totalLosses,
+      goalsFavor: team.goalsFavor + data[i].goalsFavor,
+      goalsOwn: team.goalsOwn + data[i].goalsOwn,
+      goalsBalance: team.goalsFavor + data[i].goalsOwn,
+      efficiency: +(
+        (((team.totalVictories + data[i].totalVictories) * 3
+         + (data[i].totalDraws + team.totalDraws))
+          / ((team.totalGames + data[i].totalGames) * 3)) * 100).toFixed(2),
+    }
+    ));
+    res.status(away.code).json(response);
   }
 }
 
